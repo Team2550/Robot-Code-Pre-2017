660623b3214e0f112b3e47194f818939fd29a9d5
Caleb Reister <calebreister@gmail.com>
2015-02-01 22:19:42 -0800

    Added comments to Lift implementation.

--- 2015Code/src/Lift.cc
+++ 2015Code/src/Lift.cc
@@ -1,12 +1,32 @@
 #include "Lift.hh"
 
-Lift::Lift() : liftMotor(2) { //We will probably need to change this numbers
+Lift::Lift() : liftMotor(2) { //We may need to change this number
 	liftMotor.SetPositionMode(CANJaguar::QuadEncoder, 256, 0, 0, 0);
 	//0,0,0 disables PID control
 	//Consider making 256 1 so that we don't have to deal with floating point values
 	
-	liftMotor.EnableControl(); //Maybe we should have an enable/disable method
+	/* Instead of setPositionMode, use setPercentMode configured with an encoder.
+	   See http://first.wpi.edu/FRC/roborio/release/docs/cpp/classCANJaguar.html#ab6a9023e64d35c639c366401c1c7be12
+	   Percent mode is just like running a PWM motor, but we can also read the
+	   encoder and check the limit switches via CAN.
+	*/
+	//PercentMode constructor
+	//liftMotor.SetPercentMode(CanJaguar::QuadEncoder, 256);
+	
+	/* double CANJaguar::GetPosition,
+	   bool CANJaguar::GetForwardLimitOK, and
+	   bool CANJaguar::GetReverseLimitOK 
+	   will be useful later on.
+	*/
+	
+	liftMotor.EnableControl(); //Maybe you should have an enable/disable method
 	    //for the whole lift.
+	    
+	/* We will need to mess with the robot some to figure out how to determine
+	   an initial position. I'm not sure if it is better to move the lift to its
+	   upper and lower limits first or to make sure that team mebers place it in
+	   a known position every time (most likely at the bottom.
+	*/
 
 	distanceRemaining = 0;
 	done = true;
@@ -16,19 +36,25 @@ void Lift::bottom() {
 	liftMotor.Set(0); //This may not work, I think we need to manually
 	//manage the encoder. It is going to run for multiple rotations.
 	
-	/*
-	    while (bottom limit switch is not pressed)
-	        move lift down
-	    stop motor
-	*/
 }
 
 void Lift::top() {
-	//to be written
+    /*
+    if not at top
+        move lift up
+        done = false;
+    else
+        stop motor
+        done = true;
+    update position (if necessary, we need to experiment)
+    */
+    //We will probably need to tweak exactly how we manage the done member.
+    //In order to protect the hardware, maybe we should not allow the lift to
+    //change tasks in the middle of something.
 }
 
 double Lift::getPosition() {
-	return liftMotor.GetPosition(); //this will return the currentPosition member.
+	return liftMotor.GetPosition();
 }
 
 void Lift::setPosition(double dist) {
@@ -43,7 +69,10 @@ void Lift::setPosition(double dist) {
 	*/
 }
 
+//Replace up and down with single move function accepting +-1 which determines
+//speed and direction
 void Lift::up() {
+    //rewrite
 	liftMotor.Set(liftMotor.GetPosition() + .1);
 }
 
@@ -51,8 +80,24 @@ void Lift::down() {
 	liftMotor.Set(liftMotor.GetPosition() - .1);
 }
 
-void Lift::goTo(int boxes) {
-	//also to be written
+void Lift::goTo(int boxes) { //rename to moveTo
+    //We should create a version of this function that accepts the number of
+    //ticks from the bottom to move to.
+    
+    //The box level version may be slightly more complicated, here I will write
+    //pcode for the move to position version.
+	/*
+	distRemaining = (current position) - final position
+	if distRemaining == 0 //this will create issues with a float
+	    stop
+	    done = true
+	else if (dist remainging < 0)
+	    move down
+	    done = false
+    else
+        move up
+        done = false
+	*/
 }
 
 void Lift::remoteLift(float leftStick, bool leftButton, bool rightButton) {
